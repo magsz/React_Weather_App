782d2b2791c975a4fc4eea304512dd31c9ddb885
Mario Gomez <48229266+magsz@users.noreply.github.com>
2024-12-07 22:53:01 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "/Users/magsz/Documents/VS Code/React/weather-app/src/App.js",
+    "/Users/magsz/Documents/VS Code/React/weather-app/src/index.js",
+    "/Users/magsz/Documents/VS Code/React/weather-app/public/index.html",
+    "/Users/magsz/Documents/VS Code/React/weather-app/src/Components/Favorites.js",
+    "/Users/magsz/Documents/VS Code/React/weather-app/src/Components/Forecast.js",
+    "/Users/magsz/Documents/VS Code/React/weather-app/src/Components/Search.js",
+    "/Users/magsz/Documents/VS Code/React/weather-app/src/Components/Weather.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
